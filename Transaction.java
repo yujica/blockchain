d443587a343b061976ba09ba02660791e72a1949
Yuji <35679219+yujica@users.noreply.github.com>
2019-10-09 22:51:17 +0900

    Delete unnecessary parts and returns

--- Transaction.java
+++ Transaction.java
@@ -1,34 +1,33 @@
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import java.time.*;
-import java.util.Map.Entry;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.StringJoiner;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
-import java.util.StringJoiner;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 
 class BlockChain{
-    List<Map<String, Object>> transaction_pool;
     List<Map<String, Object>> chain;
+    List<Map<String, Object>> transaction_pool;
     Map<String, Object> block;
-    String hash;
     Map<String, Object> transaction;
+    String hash;
 
     public BlockChain(){
         this.transaction_pool = new ArrayList<>();
         this.chain = new ArrayList<Map<String, Object>>();
-        create_block("0", "init hash");
+        create_block(0, "init hash");
     }
     
-    public Map<String, Object> create_block(String nonce, String previous_hash){
+    public void create_block(int nonce, String previous_hash){
 
         this.block = new HashMap<String, Object>();
         long currentTimestamp = System.currentTimeMillis();
@@ -39,12 +38,10 @@ class BlockChain{
         
         this.chain.add(block);
         this.transaction_pool = new ArrayList<>();
-
-        return block;
     }
 
     public String hash(Map<String, Object> unsorted_block){
-        String sha256 = "";
+        StringBuilder sha256 = new StringBuilder();
         String sorted_block_str;
         StringJoiner sj = new StringJoiner("");
         unsorted_block.entrySet().stream()
@@ -55,20 +52,27 @@ class BlockChain{
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] result = digest.digest(sorted_block_str.getBytes());
-            sha256 = String.format("%040x", new BigInteger(1, result));
+            for (byte b : result) {
+                sha256.append(String.format("%02x", b));
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
-        return sha256;
+        return sha256.toString();
     }
 
-    public boolean add_transaction(String sender_blockchain_address, String recipient_blockchain_address, double value){
+    public void add_transaction(String sender_blockchain_address, String recipient_blockchain_address, double value){
         this.transaction = new HashMap<String, Object>();
         transaction.put("sender_blockchain_address", sender_blockchain_address);
         transaction.put("recipient_blockchain_address", recipient_blockchain_address);
         transaction.put("value", value);
         this.transaction_pool.add(transaction);
-        return true;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T> T autoCast(Object obj) {
+        T castObj = (T) obj;
+        return castObj;
     }
 
     public void pprint(List<Map<String, Object>> blocklist){
@@ -76,30 +80,23 @@ class BlockChain{
         for (Map<String, Object> blockmap : blocklist)
         {
             System.out.println("==================== Chain " + index + " ====================");
-            System.out.println("nonce         => " + blockmap.get("nonce"));
-            System.out.println("previous_hash => " + blockmap.get("previous_hash"));
-            System.out.println("timestamp     => " + blockmap.get("timestamp"));
-            System.out.println("transactions  => " + blockmap.get("transactions"));
+            System.out.println(" nonce            => " + blockmap.get("nonce"));
+            System.out.println(" previous_hash    => " + blockmap.get("previous_hash"));
+            System.out.println(" timestamp        => " + blockmap.get("timestamp"));
+            int index_trans = 0;
+            List<Map<String, Object>> transactions_list = autoCast(blockmap.get("transactions"));
+            for (Map<String, Object> transactions_map : transactions_list) {
+            	    System.out.println("   ##### transactions " + index_trans + " #####");
+                	System.out.println("   sender_blockchain_address    => " + transactions_map.get("sender_blockchain_address"));
+                	System.out.println("   recipient_blockchain_address => " + transactions_map.get("recipient_blockchain_address"));
+                	System.out.println("   value  => " + transactions_map.get("value"));
+                	index_trans++;
+            }
             index++;
         }
         System.out.println("");
         System.out.println("*********************************************");
     }
-
-
-def pprint(chains):
-    for i, chain in enumerate(chains):
-        print(f'{"="*25} Chain {i} {"="*25}')
-        for k, v in chain.items():
-            if k == 'transactions':
-                print(k)
-                for d in v:
-                    print(f'{"-"*40}')
-                    for kk, vv in d.items():
-                        print(f' {kk:30}{vv}')
-            else:
-                print(f'{k:15}{v}')
-    print(f'{"*"*25}')
 
 }
 
@@ -119,13 +116,13 @@ public class Transaction {
         
         block_chain.add_transaction("Tom","Bob", 1.0);
         previous_hash = block_chain.hash((block_chain.chain.size()>0) ? block_chain.chain.get(block_chain.chain.size()-1) : null);
-        block_map = block_chain.create_block("5", previous_hash);
+        block_chain.create_block(5, previous_hash);
         block_chain.pprint(block_chain.chain);
         
         block_chain.add_transaction("Carol","Daisy", 2.0);
         block_chain.add_transaction("Xmen","Yuta", 3.0);
         previous_hash = block_chain.hash((block_chain.chain.size()>0) ? block_chain.chain.get(block_chain.chain.size()-1) : null);
-        block_map = block_chain.create_block("2", previous_hash);
+        block_chain.create_block(2, previous_hash);
         block_chain.pprint(block_chain.chain);
     }
 
